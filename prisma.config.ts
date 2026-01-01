4c4e215014d57742bdf9c093fbe763d1a42f61cb
alit644 <101087513+alit644@users.noreply.github.com>
2026-01-01 18:19:57 +0300

    Add DIRECT_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("DIRECT_URL"),
   },
 });
 
+//TODO : Add database connection pooling configuration
+//TODO : عرض كارت الخدمة في صفحة العرض
