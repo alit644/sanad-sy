2a6a553623afcc3308010d3cafe4b9af28d9aba5
alit644 <101087513+alit644@users.noreply.github.com>
2026-01-02 14:20:06 +0300

    Connect to Supabase via connection pooling

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,3 +1,4 @@
+
 import { PrismaClient } from '../generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
@@ -7,6 +8,7 @@ const globalForPrisma = global as unknown as {
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
+
 })
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
