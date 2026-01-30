94fe0cd43420bcaffcf6a0838e019b753e2361c8
alit644 <101087513+alit644@users.noreply.github.com>
2026-01-30 14:18:23 +0300

    fix: minor filtering issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,9 @@
-import { Search } from "lucide-react";
-import { Input } from "./ui/input";
+import { Suspense } from "react";
 import SearchInput from "./SearchInput";
 
 const Hero = () => {
- return (
-   <section className="bg-linear-to-br from-via-white via-white to-primary/25 py-12 md:py-16 border-b border-border px-4 md:px-0">
+  return (
+    <section className="bg-linear-to-br from-via-white via-white to-primary/25 py-12 md:py-16 border-b border-border px-4 md:px-0">
       <div className="container">
         <div className="max-w-2xl  mx-auto text-center">
           <h1 className="animate-fade-up text-3xl md:text-4xl font-bold text-foreground mb-4">
@@ -13,12 +12,13 @@ const Hero = () => {
           <p className="animate-fade-up-delay-1 text-muted-foreground text-base md:text-lg mb-8">
             دليل مجتمعي محدث. جميع المعلومات تخضع للمراجعة و الرقابة.
           </p>
-          
-          <SearchInput />
+          <Suspense fallback={<div>جاري التحميل...</div>}>
+            <SearchInput />
+          </Suspense>
         </div>
       </div>
     </section>
- );
-}
+  );
+};
 
 export default Hero;
